4cfb27dce4378d6c827120e4749b65190a65bfaa
Jonas Dreiplatz <jonas.dreiplatz@gyho-hattingen.de>
2025-12-03 15:12:15 +0100

    sortierte List angefangen

---
+++ anwendung_uebung/Sortierung.java
@@ -0,0 +1,34 @@
+
+public class Sortierung <Veranstaltungen>{
+  List<Veranstaltungen> sortierteListe = new List();
+
+  public List<Veranstaltungen> ermittleSortiere()
+  {
+    int max=0;
+    while(alteListe.isEmpty == false)
+    {
+      alteListe.toFirst;
+      int max=0;
+      while(alteListe.hasAccess=true){
+          if(alteListe.gibAnzahlFreiePlaetze() > max)
+          {
+            max = alteListe.gibAnzahlFreiePlaetze;
+          }
+        alteListe.next();    
+        }
+      alteListe.toFirst;  
+      while(alteListe.hasAccess=true){
+        if(alteListe.gibAnzahlFreiePlaetze() = max){
+          sortierteListe.append()=max;
+          alteListe.remove();
+          alteListe.next();  
+          }
+      }
+      
+    }                                                 
+    
+    
+    
+    
+    
+  } 
